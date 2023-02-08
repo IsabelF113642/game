fb3a59545835c0ffe5e1701f71c65ad643dd59e8
IsabelF113642 <113522322+IsabelF113642@users.noreply.github.com>
2023-02-08 08:42:37 -0500

    Click game

---
+++ pygame/click_game.py
@@ -0,0 +1,20 @@
+blob=Actor('characterman')
+blob.topright= 0,10
+
+WIDTH=500
+HEIGHT=blob.height+20
+
+def draw():
+    screen.fill((128, 0,0))
+    blob.draw()
+
+def update():
+    blob.left = blob.left +2
+    if blob.left>WIDTH:
+        blob.right=0
+
+def on_mouse_down(pos):
+    if blob.collidepoint(pos):
+        print("Ahhh!")
+    else:
+        print("Haha you missed!")
